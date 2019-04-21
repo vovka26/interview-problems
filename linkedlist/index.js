085656616de5692780d535578eac01b88d254c4c
vovka26 <317110@gmail.com>
2019-04-21 14:55:23 -0400

    forEach method linkedList

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -15,7 +15,7 @@ class LinkedList {
     }
 
     insertFirst(data){
-        this.head = new Node(data, this.head)
+        this.head = new Node(data, this.head);
     }
 
     size(){
@@ -150,7 +150,16 @@ class LinkedList {
         const previous = this.getAt(int - 1) || this.getLast();
         const node = new Node(data, previous.next);
         previous.next = node;
+    }
 
+    forEach(fn){
+        let node = this.head;
+        let counter = 0;
+        while(node){
+            fn(node, counter);
+            node = node.next;
+            counter++;
+        }
     }
 
 }
