144b11fb85c378855fde618a6d329b7a222860a5
vovka26 <317110@gmail.com>
2019-04-21 20:10:22 -0400

    traverseBF and traverseDF methods

--- tree/index.js
+++ tree/index.js
@@ -10,8 +10,45 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+    constructor(data){
+        this.data = data;
+        this.children = [];
+    }
 
-class Tree {}
+    add(data){
+        this.children.push(new Node(data));
+    }
+
+    remove(data){
+       this.children = this.children.filter(node => node.data !== data);
+    }
+}
+
+class Tree {
+    constructor(){
+        this.root = null;
+    }
+
+    traverseBF(fn){
+        const array = [this.root];
+        while (array.length){
+            const node = array.shift();
+
+            array.push(...node.children);
+            fn(node);
+        }
+    }
+
+    traverseDF(fn){
+        const array = [this.root];
+        while(array.length){
+            const node = array.shift();
+
+            array.unshift(...node.children);
+            fn(node);
+        }
+    }
+}
 
 module.exports = { Tree, Node };
